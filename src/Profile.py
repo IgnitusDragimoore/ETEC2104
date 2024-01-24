4c4d3cfd38f6450f477cee67cacfebba322a1abd
IgnitusDragimoore <hunterrcable@gmail.com>
2024-01-24 09:20:08 -0500

    Everything else

---
+++ src/Profile.py
@@ -0,0 +1,46 @@
+import tornado.web
+import random
+
+accountData= {
+    "alice" : {
+        "name": "Alice Smith",
+        "dateOfBirth": "January 1st",
+        "email" : "alice@example.com"
+    },
+
+    "bob" : {
+        "name": "Bob Jones",
+        "dateOfBirth": "December 31st",
+        "email" : "bob@bob.xyz"
+    },
+
+    "carol" : {
+        "name": "Carol Ling",
+        "dateOfBirth": "July 17th",
+        "email" : "carol@example.com"
+    },
+
+    "dave" : {
+        "name": "Dave N. Port",
+        "dateOfBirth": "March 14th",
+        "email" : "dave@dave.dave"
+    }
+}
+
+
+class Handler(tornado.web.RequestHandler):
+    def get(self):
+        p = self.request.path
+        username = p.split("/")[2]
+        realName = accountData [username]["name"]
+        birthDate = accountData [username]["dateOfBirth"]
+        email = accountData [username]["email"]
+
+        self.render("profile.html",
+                    realName = realName,
+                    birthDate = birthDate,
+                    email = email
+                    )
+
+
+ 
--- src/main.py
+++ src/main.py
@@ -1,18 +1,28 @@
 import asyncio
+import os, os.path
 import tornado.web
+import Index, Quote, TemplateTest, Profile
 
-class IndexPage(tornado.web.RequestHandler):
-    def get(self):
-        self.write("I like cheese!")
+HTMLDIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..","html"
+    )
+)
 
 def makeApp():
-    endpoints = [
-        ("/", IndexPage)
+    endpoints=[
+        ("/",Index.Handler),
+        ("/quote",Quote.Handler),
+        ("/fancy",TemplateTest.Handler),
+        ("/profile/.*",Profile.Handler)
     ]
-    app = tornado.web.Application(endpoints)
+    app = tornado.web.Application(
+        endpoints,
+        static_path=HTMLDIR
+    )
     app.listen(8000)
     return app
-
 
 if __name__ == "__main__":
     app = makeApp()
